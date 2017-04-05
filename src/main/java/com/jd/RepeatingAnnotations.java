0d90feb444ae3253ecec1135a24d1462440c6038
buxiaoming <buxiaoming@jd.com>
2017-04-05 18:22:36 +0800

    重复注解

---
+++ src/main/java/com/jd/RepeatingAnnotations.java
@@ -0,0 +1,40 @@
+package com.jd;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: buxiaoming
+ * Date: 2017/4/5
+ * Time: 18:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class RepeatingAnnotations {
+    @Target( ElementType.TYPE )
+    @Retention( RetentionPolicy.RUNTIME )
+    public @interface Filters {
+        Filter[] value();
+    }
+
+    @Target( ElementType.TYPE )
+    @Retention( RetentionPolicy.RUNTIME )
+    @Repeatable( Filters.class )
+    public @interface Filter {
+        String value();
+    };
+
+    @Filter( "filter1" )
+    @Filter( "filter2" )
+    public interface Filterable {
+    }
+
+    public static void main(String[] args) {
+        for( Filter filter: Filterable.class.getAnnotationsByType( Filter.class ) ) {
+            System.out.println( filter.value() );
+        }
+    }
+}
